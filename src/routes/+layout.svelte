186b3dd224a0feffe60cf491a7de92ddd8f3b3f6
Gamzee <lithiumturkin@protonmail.com>
2024-02-16 11:57:07 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { AppShell } from '@skeletonlabs/skeleton';
+	import { AppBar } from '@skeletonlabs/skeleton';
+	import '../app.postcss';
+</script>
+
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+			<svelte:fragment slot="lead">
+				<img alt="Raccoon eating cheese" src="images/ma_cheese.png" class="w-14 rounded-xl"/>
+			</svelte:fragment>
+			<p class="text-xl">TicTacToe</p>
+			<svelte:fragment slot="trail">
+				<button type="button" class="btn variant-filled rounded-xl">
+					reset
+				</button>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+
+	<slot />
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	let squares = new Array(9).fill(null)
+	let xIsNext = true;
+	$: status = "Next Player: " + (xIsNext ? "X" : "0")
+
+
+</script>
+
+<div class="h-full flex items-center justify-center">
+	  <div class="grid grid-cols-3">
+		{#each squares as square, i}
+		  <button on:click={()=>{setValue(i)}} type="button" class="btn variant-ringed-tertiary size-32 flex items-center justify-center rounded-none">
+			{square?square:""}
+		</button>
+		{/each}
+	  </div>
+</div>
+  
