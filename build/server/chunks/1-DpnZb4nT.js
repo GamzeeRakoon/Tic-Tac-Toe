dca81db3d3ce9d5be0f4e0b09d4c127279f3e813
Gamzee <lithiumturkin@protonmail.com>
2024-03-14 12:59:16 +0100

    Revert "Fix: work around vercel bug"
    This reverts commit 372e7052becdbee56255ca5caa97f15bd1d0c9e2.

--- build/server/chunks/1-DpnZb4nT.js
+++
@@ -1,9 +0,0 @@
-const index = 1;
-let component_cache;
-const component = async () => component_cache ??= (await import('./error.svelte-Dg6Wso2m.js')).default;
-const imports = ["_app/immutable/nodes/1.DsNJ4wCD.js","_app/immutable/chunks/scheduler.cwPrGksG.js","_app/immutable/chunks/index.csaw_Ye6.js","_app/immutable/chunks/stores.CYkGZrFy.js","_app/immutable/chunks/entry.DNdkb8mG.js","_app/immutable/chunks/index.-__t-DoX.js"];
-const stylesheets = [];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=1-DpnZb4nT.js.map
--- build/server/chunks/2-6iLTQBy0.js
+++
@@ -1,9 +0,0 @@
-const index = 2;
-let component_cache;
-const component = async () => component_cache ??= (await import('./_page.svelte-CqnSdZPk.js')).default;
-const imports = ["_app/immutable/nodes/2.BF2X-aZk.js","_app/immutable/chunks/scheduler.cwPrGksG.js","_app/immutable/chunks/index.csaw_Ye6.js","_app/immutable/chunks/index.1HjZlvs3.js","_app/immutable/chunks/store.DzbeFD6T.js","_app/immutable/chunks/index.-__t-DoX.js","_app/immutable/chunks/entry.DNdkb8mG.js"];
-const stylesheets = ["_app/immutable/assets/store.Cirlo5Z8.css"];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=2-6iLTQBy0.js.map
